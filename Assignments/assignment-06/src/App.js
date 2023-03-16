6f5c8070563bc03dc07bac18b378ad93e86e0a5f
DONELSINHA01 <100290944+DONELSINHA01@users.noreply.github.com>
2023-03-17 00:21:24 +0530

    Add files via upload

---
+++ Assignments/assignment-06/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main.jsx';
+import Footer from './Components/Footer';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
